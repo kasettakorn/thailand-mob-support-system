5e69999b9f7b24d99882091386336f9e9b092029
kasettakorn <kasettakorn_techno@hotmail.com>
2020-10-20 23:54:12 +0700

    Updates

---
+++ precache-manifest.69f2ac11bb6f384b8f8d1af79a09b7b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fc23515a347214fcafed07b27150e9",
+    "url": "/thailand-mob-support-system/index.html"
+  },
+  {
+    "revision": "0641cc2cb77d3caf93c7",
+    "url": "/thailand-mob-support-system/static/css/2.18a54708.chunk.css"
+  },
+  {
+    "revision": "5dc29881bd69174494b3",
+    "url": "/thailand-mob-support-system/static/css/main.e2818288.chunk.css"
+  },
+  {
+    "revision": "0641cc2cb77d3caf93c7",
+    "url": "/thailand-mob-support-system/static/js/2.55c15a71.chunk.js"
+  },
+  {
+    "revision": "04c86c67cfe017e6a2e6aa9d7adbce4c",
+    "url": "/thailand-mob-support-system/static/js/2.55c15a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc29881bd69174494b3",
+    "url": "/thailand-mob-support-system/static/js/main.e9c0eef8.chunk.js"
+  },
+  {
+    "revision": "4069d95121b33408a950",
+    "url": "/thailand-mob-support-system/static/js/runtime-main.0b7221c6.js"
+  },
+  {
+    "revision": "7c9810df320391e9552c48c3e7a3ac6d",
+    "url": "/thailand-mob-support-system/static/media/pin.7c9810df.svg"
+  }
+]);
